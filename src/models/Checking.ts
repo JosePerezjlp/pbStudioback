db84f280363166d657201045a463054b6fafc6f7
henvisi-1994 <henvisi1994@gmail.com>
2025-09-09 17:46:22 -0500

    feat(WellHub): :white_check_mark: Checking wellhub and class creation

--- src/models/Checking.ts
+++ src/models/Checking.ts
@@ -1,6 +1,8 @@
 export class Cheking {
-  gympass_user_id: string | number |null;
-  product_id: string | number |null;
+  gympass_user_id: string | number | null;
+
+  product_id: string | number | null;
+
   constructor() {
     this.gympass_user_id = null;
     this.product_id = null;
