7380fcb7d01f6c83215f6c3a4034fe102c64b8ac
henvisi-1994 <henvisi1994@gmail.com>
2025-09-09 17:46:22 -0500

    feat(WellHub): :white_check_mark: Checking wellhub and class creation

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import admin from "../config/firebase";
+import { GympassService } from "../services/gympass.service";
+import { Cheking } from "../models/Checking";
 
 
 type DocData = Record<string, unknown>;
